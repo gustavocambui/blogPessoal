3ca397d4a13918e1889e1d96e5628362bec50839
Gustavo <jgustavocambui@gmail.com>
2022-08-17 10:53:59 -0300

    alterações

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -38,7 +38,7 @@ function Home() {
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="titulo">Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo">Expresse aqui os seus pensamentos e opiniões!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo">Blog voltado para cultura pop. Compartilhe aqui reviews e comentários sobre séries e filmes.</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
